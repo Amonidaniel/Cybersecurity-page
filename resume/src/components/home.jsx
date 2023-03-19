4e9d2cca81ac1382414ad780344d5397074e616b
Amonidaniel <danielamoni1410@gmail.com>
2023-03-19 17:37:30 -0400

    cybersecurity final

--- resume/src/components/home.jsx
+++ resume/src/components/home.jsx
@@ -6,16 +6,16 @@ const Home = () => {
      <div>
         <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
           <div
-            className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+            className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-black shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
             aria-hidden="true"
           />
           <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-              <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+              <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-white sm:text-6xl lg:col-span-2 xl:col-auto">
                 Daniel Amoni, CyberSecurity professional.
               </h1>
               <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-                <p className="text-lg leading-8 text-gray-600">
+                <p className="text-lg leading-8 text-gray-200">
                 Cybersecurity professional with strong knowledge about web pages architecture and programming languages. Proficiency with Web Penetration and Ethical Hacking Tools such as Kali Linux, Nmap, Nessus, Metasploit Framework Structure (MFS)
                 <br />
                 <br />
@@ -41,7 +41,7 @@ o	Penetration testing tools variety (Burpsuite, Nmap, MSFVenom, Metasploit Frame
                 </div>
               </div>
               <img
-                src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
+                src="picture.jpg"
                 alt=""
                 className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
               />
