cdca0851687cab89982d28de6df165c0b7296fa2
Amonidaniel <danielamoni1410@gmail.com>
2023-03-19 12:04:43 -0400

    cybersecurity page

---
+++ resume/src/App.js
@@ -0,0 +1,13 @@
+import Certificates from "./components/certificates";
+import Home from "./components/home";
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Certificates/>
+    </div>
+  );
+}
+
+export default App;
